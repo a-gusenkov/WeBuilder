fc76bdc2b525165e1c44f98c5dd7e6b55205e5ab
Liyu Lin <lliyu.brooklyn@gmail.com>
2021-11-11 18:47:06 -0500

    adding the education layouts

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-d
 import HomePage from './pages/homePage';
 import Categories from './pages/categories';
 import AboutMe from './layouts/aboutMe';
+import Education from './layouts/education';
 
 
 import './App.css';
@@ -18,6 +19,7 @@ function App() {
         <Route exact path="/homePage" component={HomePage} />
         <Route exact path="/categories" component={Categories} />
         <Route exact path="/aboutMe" component={AboutMe} />
+        <Route exact path="/education" component={Education} />
       </Switch>
     </Router>
 
--- src/layouts/aboutMe.js
+++ src/layouts/aboutMe.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { Card, Stack } from 'react-bootstrap';
-
+import { Card } from 'react-bootstrap';
 
 
 class AboutMe extends Component {
@@ -18,6 +17,7 @@ class AboutMe extends Component {
           bgColor_One: "rgba(30, 139, 195, 1)",
           bgColor_Two: "rgba(153, 42, 42, 0.01)"
         })
+        console.log("You chose the Layout1");
     }
 
     boxClick_Two = (e) => {
@@ -25,15 +25,15 @@ class AboutMe extends Component {
           bgColor_One: "rgba(153, 42, 42, 0.01)",
           bgColor_Two: "rgba(30, 139, 195, 1)"
         })
+        console.log("You chose the Layout2");
     }
 
     render() {
     return (
-        <div>
-        <Stack gap={2}>
+    <div>
         <div className="aboutMe" style={{backgroundColor: this.state.bgColor_One}}
            onClick={this.boxClick_One}>
-            <Card hoverable>
+            <Card style={{backgroundColor:'#C2A5FF'}}>
                 <div className="row no-gutters">
                     <div className="col">
                         <img src="https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-4.jpg" className="img-fluid" alt="" width="100%" height="100%"></img>
@@ -47,12 +47,10 @@ class AboutMe extends Component {
                     </div>
                 </div>
             </Card>
-        </div>
-           
-
+        </div>    
         <div className="aboutMe" style={{backgroundColor: this.state.bgColor_Two}}
            onClick={this.boxClick_Two}>
-            <Card hoverable>
+            <Card style={{backgroundColor:'#C2A5FF'}}>
                 <div className="row no-gutters">
                     <div className="col-7">
                     <div className="card-block px-2">
@@ -66,8 +64,7 @@ class AboutMe extends Component {
                     </div>
                 </div>
             </Card>
-        </div>
-        </Stack>
+        </div>   
     </div>
     );
     }
